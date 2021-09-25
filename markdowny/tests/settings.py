cb2dae587da76dc41b876d5e37ea3882f1cacf99
Richard Cornish <richardcornish@gmail.com>
2021-09-24 18:03:43 -0700

    Updated settings with newer syntax

--- markdowny/tests/settings.py
+++ markdowny/tests/settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "fake-key"
 
@@ -12,7 +12,7 @@ INSTALLED_APPS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": BASE_DIR / "db.sqlite3",
         "TEST": {
             "NAME": None,
         },
