058c0f91edc7a7fc44e55c36ce408b42af6aa596
Richard Cornish <richardcornish@gmail.com>
2021-09-24 18:12:21 -0700

    Reverted settings to older syntax

--- markdowny/tests/settings.py
+++ markdowny/tests/settings.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+import os
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = "fake-key"
 
@@ -12,7 +12,7 @@ INSTALLED_APPS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         "TEST": {
             "NAME": None,
         },
